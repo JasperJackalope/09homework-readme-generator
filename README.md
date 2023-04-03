88726ec6b27eba60e03e664dde100782e96970a0
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-03 16:15:14 -0700

    Forgot to update generateMarkdown from local

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Poop
+# Test
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+Test
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## License
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+## Installation
+
+Test
+
+## Usage
+
+Test
+
+## Contributing
+
+Test
+
+## Tests
+
+Test
+
+## Questions
+
+For questions, contact me at Test. My GitHub username is Test.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,123 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+
+  let badge;
+  switch (license) {
+    case 'MIT':
+      badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'GNU GPL':
+      badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    case 'Apache':
+      badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'CC-BY-SA':
+      badge = '[![License: CC-BY-SA](https://licensebuttons.net/l/by-sa/3.0/88x31.png)](https://creativecommons.org/licenses/by-sa/3.0/)';
+      break;
+    case 'ISC':
+      badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+      break;
+    default:
+      badge = '';
+      break;
+  }
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  let link;
+  switch (license) {
+    case 'MIT':
+      link = 'https://opensource.org/licenses/MIT';
+      break;
+    case 'GNU GPL':
+      link = 'https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    case 'Apache':
+      link = 'https://opensource.org/licenses/Apache-2.0';
+      break;
+    case 'CC-BY-SA':
+      link = 'https://creativecommons.org/licenses/by-sa/3.0/';
+      break;
+    case 'ISC':
+      link = 'https://opensource.org/licenses/ISC';
+      break;
+    default:
+      link = '';
+      break;
+  }
+
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  const link = renderLicenseLink(license);
+  return `## License
+
+This project is licensed under the [${license}](${link}) license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
+${licenseBadge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+${licenseSection}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For questions, contact me at ${data.questions2}. My GitHub username is ${data.questions}.
 `;
 }
+
 
 module.exports = generateMarkdown;
