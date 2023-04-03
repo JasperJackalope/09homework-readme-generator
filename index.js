e64252017025d899c9811b6d96ec0be6bc7b3100
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-02 22:13:55 -0700

    Skeleton Functions Added for writetofile and init

--- index.js
+++ index.js
@@ -55,10 +55,32 @@ const questions = [
   
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('README file generated successfully!');
+    });
+  }
+  
 
 // TODO: Create a function to initialize app
-function init() {}
+
+function init() {
+    inquirer.prompt(questions)
+      .then(data => {
+        const fileName = `${data.title.toLowerCase().split(' ').join('-')}.md`;
+        const readmeContent = generateReadme(data);
+        writeToFile(fileName, readmeContent);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+  
 
 // Function call to initialize app
 init();
